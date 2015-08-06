4871408ceeaafd6404521f2cad78a3c19c701b8f
sub77 <sub77@ymail.com>
2015-08-06 21:59:32 +0200

    kernel config to device

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 TARGET_KERNEL_SOURCE := kernel/samsung/matisse
 #TARGET_KERNEL_CONFIG := msm8226-sec_defconfig
-TARGET_KERNEL_CONFIG := matissewifi_defconfig
+#TARGET_KERNEL_CONFIG := matissewifi_defconfig
 #TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 
 # Lights
